8492bd7ff6a6071b63f92b17e303c74a9b03b426
Abhishek-2400 <111686252+Abhishek-2400@users.noreply.github.com>
2023-03-15 21:01:04 +0530

    Delete Spam-Messages directory

--- Spam-Messages/ytspam/ytspamapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class YTSpamClassifier(models.Model):
-    text = models.TextField()
-    label = models.CharField(max_length=100)
--- Spam-Messages/ytspam/ytspamapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-from .views import classify_text
-
-urlpatterns = [
-    path('', classify_text, name='classify_text'),
-]
